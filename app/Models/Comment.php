4d4da992b6076ed9cb43918cc7fff48b1bf8dba8
prospermbuma <prospermbuma@gmail.com>
2025-05-27 00:33:19 +0300

    Updated Post and Comment Models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
-    //
+      protected $connection = 'mongodb';
+
+    protected $table = 'comments';
+
+    protected $fillable = [
+        'comments',
+        'user_id',
+        'post_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(related: User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(related: Comment::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(related: Like::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
-    //
+    protected $connection = 'mongodb';
+
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'caption',
+        'image_path',
+        'image_url',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(related: User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(related: Comment::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(related: Like::class);
+    }
 }
