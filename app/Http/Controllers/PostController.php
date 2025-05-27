589df8756af24e4e85c0330aaa3734b56633058f
prospermbuma <prospermbuma@gmail.com>
2025-05-28 01:20:16 +0300

    Updated contents of PostContoller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,96 +5,86 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
 
 class PostController extends Controller
 {
-  
-    // Constructor
     public function __construct()
     {
-        $this->middleware(middleware: 'auth');
+        $this->middleware('auth');
     }
 
-    // Getter - Get all post sort by newest first
-    public function index(): View
+    public function index()
     {
-        $posts = Post::with(relations: 'user')->latest()->get();
-        return view(view: 'posts.index', data: compact(var_name: 'posts'));
+        $posts = Post::with('user')->latest()->get();
+        return view('posts.index', compact('posts'));
     }
 
-    // Getter - Create post
-    public function create(): View
+    public function create()
     {
-        return view(view: 'posts.create');
+        return view('posts.create');
     }
 
-    // Setter - Store new post
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
-        $data = $request->validate(rules: [
+        $data = $request->validate([
             'caption' => 'required',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
 
-        $image_path = $request->file(key: 'image')->store(path: 'uploads', options: 'public');
+        $imagePath = $request->file('image')->store('uploads', 'public');
 
-        auth()->user()->posts()->create(attributes: [
+        auth()->user()->posts()->create([
             'caption' => $data['caption'],
-            'image_path' => $image_path,
+            'image_path' => $imagePath,
         ]);
 
-        return redirect(to: 'profile/' .auth()->user()->id);
+        return redirect('/profile/' . auth()->user()->id);
     }
 
-    // Getter - Get a post
-    public function show(Post $post): View
+    public function show(Post $post)
     {
-        return view(view: 'posts.show', data: compact(var_name: 'post'));
+        return view('posts.show', compact('post'));
     }
 
-    // Getter - Edit post
     public function edit(Post $post)
     {
-        // Check if the authentcated user is the same as the post user
+        // Check if the authenticated user is the same as the post user
         if (auth()->id() !== $post->user_id) {
-            abort(code: 403, message: 'Unauthorized action.');
+            abort(403, 'Unauthorized action.');
         }
-        return view(view: 'posts.edit', data: compact(var_name: 'post'));
+        
+        return view('posts.edit', compact('post'));
     }
 
-    // Getter - Update post
-    public function update(Request $request, Post $post): RedirectResponse
+    public function update(Request $request, Post $post)
     {
-        // Check if the authentcated user is the same as the post user
+        // Check if the authenticated user is the same as the post user
         if (auth()->id() !== $post->user_id) {
-            abort(code: 403, message: 'Unauthorized action.');
+            abort(403, 'Unauthorized action.');
         }
-
-        $data = $request->validate(rules: [
+        
+        $data = $request->validate([
             'caption' => 'required',
         ]);
 
-        $post->update(attributes: $data);
+        $post->update($data);
 
-        return redirect(to: '/posts/' . $post->id);
+        return redirect('/posts/' . $post->id);
     }
 
-    // Getter - Delete post
-    public function destroy(Post $post): RedirectResponse
+    public function destroy(Post $post)
     {
-         // Check if the authentcated user is the same as the post user
+        // Check if the authenticated user is the same as the post user
         if (auth()->id() !== $post->user_id) {
-            abort(code: 403, message: 'Unauthorized action.');
+            abort(403, 'Unauthorized action.');
         }
-
+        
         // Delete the image file
-        Storage::disk(name: 'public')->delete(paths: $post->image_path);
-
+        Storage::disk('public')->delete($post->image_path);
+        
         // Delete the post
         $post->delete();
 
-        return redirect(to: 'profile/' .auth()->user()->id);
+        return redirect('/profile/' . auth()->user()->id);
     }
 }
